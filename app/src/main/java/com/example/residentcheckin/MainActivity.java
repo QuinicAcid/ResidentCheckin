f997b461c16f7ddc0d7997785f63a901cf10dc00
westw2305 <kayhegee@gmail.com>
2019-04-27 14:53:41 -0700

    first screen

--- app/src/main/java/com/example/residentcheckin/MainActivity.java
+++ app/src/main/java/com/example/residentcheckin/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.residentcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     TextView txt;
+    Button btn;
+    Button btn2;
+    EditText edtTxt;
+    String st;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtTxt = (EditText) findViewById(R.id.editText);
+        txt = (TextView) findViewById(R.id.txtResults);
+        btn = (Button) findViewById(R.id.btnSearch);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
     }
 }

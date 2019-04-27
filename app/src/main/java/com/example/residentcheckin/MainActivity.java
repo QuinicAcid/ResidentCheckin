c502c72f4e0b18bde4a922f7771f3f8d270395d6
westw2305 <kayhegee@gmail.com>
2019-04-27 13:58:42 -0700

    4/27/19

--- app/src/main/java/com/example/residentcheckin/MainActivity.java
+++ app/src/main/java/com/example/residentcheckin/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.residentcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

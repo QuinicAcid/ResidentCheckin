ae4f5e8c98790ec0227cc972711101174e6ba775
westw2305 <kayhegee@gmail.com>
2019-04-28 11:56:45 -0400

    new buttons

--- app/src/main/java/com/example/residentcheckin/MainActivity.java
+++ app/src/main/java/com/example/residentcheckin/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.residentcheckin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewDebug;
 import android.widget.Button;
@@ -11,39 +12,31 @@ import android.widget.TextView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    TextView txt;
-    Button btn;
-    Button btn2;
-    EditText edtTxt;
-    String st;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btnNext;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.btnSearch);
-        Button button2;
-        button2 = (Button) findViewById(R.id.btnNext);
-        button.setOnClickListener (new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),VerifyStatus.class);
-                startActivity(i);
+    protected void onCreate(Bundle savedIntanceState) {
+        super.onCreate(savedIntanceState);
+         Button btnSearch = findViewById(R.id.btnSearch);
+         Button btnNext = findViewById(R.id.btnNext);
 
-                setContentView(R.layout.activity_main);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
+         btnNext.setOnClickListener(this);
+         btnSearch.setOnClickListener(this);
 
-            public void onClick(View v) {
-                Intent myintent2 = new Intent(MainActivity.this,Button.class);
-                startActivity(myintent2);
+         btnNext.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
 
-            }
-        });
-
+             }
+         });
 
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent myIntent = new Intent(getBaseContext(),   VerifyStatus.class);
+        startActivity(myIntent);
+    }
 }
--- app/src/main/java/com/example/residentcheckin/VerifyStatus.java
+++ app/src/main/java/com/example/residentcheckin/VerifyStatus.java
@@ -2,13 +2,24 @@ package com.example.residentcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VerifyStatus extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_status);
+        setContentView(R.layout.activity_main);
     }
+        //public  void previous(View view){
+       // startActivity(new Intent(VerifyStatus.this,MainActivity.class));
+      // }
+
 }
+
+
+
+
+

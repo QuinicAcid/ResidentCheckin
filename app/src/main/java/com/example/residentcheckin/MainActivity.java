1224ac667f7c66cb7d1fef77a5ccad6d02b64c3d
QuinicAcid <madica@yahoo.com>
2019-04-29 00:50:24 -0400

    Created the send button and text field. Going to be used to send the user id along.

--- app/src/main/java/com/example/residentcheckin/MainActivity.java
+++ app/src/main/java/com/example/residentcheckin/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.residentcheckin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.ResidentCheckin.MESSAGE";
     TextView txt;
     EditText edtTxt;
 
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // No idea what's going on from here...
         Button button = (Button) findViewById(R.id.btnSearch);
         Button button2;
         button2 = (Button) findViewById(R.id.btnNext);
@@ -37,8 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        // ...to here. In particular, why is it located here?
+    }
 
-
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, CurrentStatus.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }

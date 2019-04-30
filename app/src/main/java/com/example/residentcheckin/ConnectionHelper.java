c61063a17326cd4c8fbd5a07d1d6a06d78af2f95
QuinicAcid <madica@yahoo.com>
2019-04-29 22:48:59 -0400

    Created ConnectionHelper

--- app/src/main/java/com/example/residentcheckin/ConnectionHelper.java
+++ app/src/main/java/com/example/residentcheckin/ConnectionHelper.java
@@ -1,4 +1,53 @@
 package com.example.residentcheckin;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionHelper {
+
+    String ip,db,DBUserNameStr,DBPasswordStr;
+
+
+    @SuppressLint("NewApi")
+    public Connection connectionclasss()
+    {
+
+        // Declaring Server ip, username, database name and password
+        ip = "SQL5008.site4now.net";
+        db = "DB_A47087_smgroup";
+        DBUserNameStr = "DB_A47087_smgroup_admin";
+        DBPasswordStr = "ftccgroup1";
+        // Declaring Server ip, username, database name and password
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        java.sql.Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip +";databaseName="+ db + ";user=" + DBUserNameStr+ ";password=" + DBPasswordStr + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
+    }
+
 }
